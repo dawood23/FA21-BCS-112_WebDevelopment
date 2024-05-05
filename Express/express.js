1dc92ffd46eeef381bacce5725b0de66b5ecdba1
Dawood Nadeem <80353999+dawood23@users.noreply.github.com>
2024-05-05 15:55:53 +0500

    express routers
    routers

--- Express/express.js
+++ Express/express.js
@@ -1,10 +1,20 @@
 const express=require('express')
 const server=express()
+const mongoose = require("mongoose");
 
-server.use(express.static("public"));
+server.use(express.static("public"))
 server.set("view engine","ejs")
 
 server.get('/check',(req,res)=>{
     res.render("check")
 })
-server.listen(3000)
+
+const itemRouter=require('./routes/item')
+
+server.use("/items",itemRouter)
+
+server.listen(3000,()=>{
+    console.log("Started Listening At 3000")
+}
+)
+
---
+++ Express/routes/item.js
@@ -0,0 +1,54 @@
+const express=require('express')
+const router=express.Router()
+
+router.get("/",(req,res)=>{
+    res.send("This is the items list")
+})
+
+router.get("/new",(req,res)=>{
+    res.send("This is the new item")
+})
+
+router.post("/",(req,res)=>{
+    res.send("Create new item")
+})
+
+router.route("/:id").get((req,res)=>{
+        id=req.params.id
+        res.send(`Getting item with id ${req.params.id} \n The item is : ${res.item}`)
+    }).put((req,res)=>{
+            id=req.params.id
+            res.send(`Updating item with id ${req.params.id}`)
+        })
+    .delete((req,res)=>{
+                id=req.params.id
+                res.send(`Deleting item with id ${req.params.id}`)
+    })
+
+const itemsList=['burger','fries','fried chicken','pizza']
+
+router.param("id",(req,res,next,id)=>{
+    if(itemsList[id]===undefined){
+        res.item="There is no item with this id"
+    }
+    else{
+    res.item=itemsList[id]
+    }
+    next()
+})
+// router.get("/:id",(req,res)=>{
+//     id=req.params.id
+//     res.send(`Getting user with id ${req.params.id}`)
+// })
+
+// router.put("/:id",(req,res)=>{
+//     id=req.params.id
+//     res.send(`Updating user with id ${req.params.id}`)
+// })
+
+// router.delete("/:id",(req,res)=>{
+//     id=req.params.id
+//     res.send(`Deleting user with id ${req.params.id}`)
+// })
+
+module.exports=router
