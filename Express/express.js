e6beacf69aea1f571d69f82bb252ffbdfecd766a
Dawood Nadeem <80353999+dawood23@users.noreply.github.com>
2024-05-13 23:38:03 +0500

    ejs.

--- Express/express.js
+++ Express/express.js
@@ -1,33 +1,47 @@
-const express=require('express')
-const server=express()
+const express = require("express");
+const server = express();
 const mongoose = require("mongoose");
+let ejsLayouts = require("express-ejs-layouts");
 
-const func=async()=>{
-    const connection= await mongoose
-      .connect("mongodb+srv://dawood:03348757377d@cluster0.nlusybm.mongodb.net/test")
-      .then(() => {
-        console.log("DB Connected");
-      })
-      .catch((err) => {
-        console.log("Unable to connect",err);
-      });
- 
-    }
+const func = async () => {
+  const connection = await mongoose
+    .connect(
+      "mongodb+srv://dawood:03348757377d@cluster0.nlusybm.mongodb.net/test"
+    )
+    .then(() => {
+      console.log("DB Connected");
+    })
+    .catch((err) => {
+      console.log("Unable to connect", err);
+    });
+};
 
-func().then(()=>{
-server.use(express.static("public"))
-server.set("view engine","ejs")
+func().then(() => {
+  server.set("view engine", "ejs");
+  server.use(express.static("public"));
+  server.use(ejsLayouts);
 
-server.get('/check',(req,res)=>{
-    res.render("check")
-})
+  server.get("/", (req, res) => {
+    res.render("home", { title: "Home" });
+  });
 
-const itemRouter=require('./routes/item')
+  server.get("/about", (req, res) => {
+    res.render("about", { title: "About Us" });
+  });
 
-server.use("/items",itemRouter)
+  server.get("/contact", (req, res) => {
+    res.render("contact", { title: "Contact Us" });
+  });
 
-server.listen(3000,()=>{
-    console.log("Started Listening At 3000")
-}
-)
-})
+  server.get("/check", (req, res) => {
+    res.render("check");
+  });
+
+  const itemRouter = require("./routes/item");
+
+  server.use("/items", itemRouter);
+
+  server.listen(5000, () => {
+    console.log("Started Listening At 3000");
+  });
+});
