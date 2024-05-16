2da6bcd28f0a4482536e0080ca121204f0779f13
Dawood Nadeem <80353999+dawood23@users.noreply.github.com>
2024-05-17 02:08:29 +0500

    bcrypt

--- Project/routes/users.js
+++ Project/routes/users.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 router.use(express.static("public"));
+
 router.get("/Login", (req, res) => {
   res.render("Login", { layout: false });
+});
+
+router.post("/Login", async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.redirect("/SignUp");
+  if (user.password != req.body.password) return res.redirect("/user/Login");
+  console.log(user);
+  // req.session.user = user;
+  return res.redirect("/");
 });
 
 router.get("/SignUp", (req, res) => {
@@ -12,11 +23,19 @@ router.get("/SignUp", (req, res) => {
 });
 
 router.post("/SignUp", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  let hashpassword = await bcrypt.hash(req.body.password, salt);
   try {
     console.log(req.body);
-    const user = new User(req.body);
+    const user = new User({
+      name: req.body.name,
+      location: req.body.location,
+      email: req.body.email,
+      password: hashpassword,
+      number: req.body.number,
+    });
     await user.save();
-    res.redirect("/");
+    res.redirect("/Login");
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Failed to create user" });
