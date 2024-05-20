78048a9692e059e5b9cdb8bd7eac3efff216ca9a
Dawood Nadeem <80353999+dawood23@users.noreply.github.com>
2024-05-21 00:43:54 +0500

    orders
    orders model, fetching data and orders.ejs

--- Project (FoodApp)/express.js
+++ Project (FoodApp)/express.js
@@ -9,6 +9,8 @@ let checkAuth = require("./middlewares/checkAuth");
 let mainMiddleware = require("./middlewares/mainMiddleware");
 let initmid = require("./middlewares/init-middleware");
 let item = require("./models/items");
+let Order = require("./models/order");
+
 connectMongo();
 
 server.set("view engine", "ejs");
@@ -73,13 +75,52 @@ server.get("/cart", checkAuth, async (req, res) => {
     }
     Total += parseInt(price) * parseInt(quantity);
   }
-
+  res.cookie("order", [itemsArr, Total]);
+  console.log([itemsArr, Total]);
   res.render("cart", { items: itemsArr, total: Total });
 });
 
-server.get("/placeorder", (req, res) => {
-  res.clearCookie("cart").send("Thankyou For Placing Your Order!!!");
-  return res.redirect("/");
+server.get("/orders", mainMiddleware, async (req, res) => {
+  try {
+    // Query the database to fetch orders for the specific user
+    const orders = await Order.find({ user: req.session.user._id }).populate(
+      "items.item"
+    );
+
+    // Render the EJS template with orders data
+    res.render("orders", { orders });
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+server.get("/placeorder", async (req, res) => {
+  try {
+    const orderData = req.cookies.order;
+    const [itemsArr, totalPrice] = orderData;
+
+    const order = new Order({
+      user: req.session.user._id,
+      status: "Pending",
+      items: itemsArr.map(([item, quantity, price]) => ({
+        item: item._id,
+        quantity: parseInt(quantity),
+        price: parseInt(price),
+      })),
+      totalPrice: parseInt(totalPrice),
+    });
+
+    await order.save();
+
+    res.clearCookie("order");
+    res.clearCookie("cart");
+
+    res.status(201).json({ message: "Order placed successfully", order });
+  } catch (error) {
+    console.error("Error placing order:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 server.listen(5000, () => {
